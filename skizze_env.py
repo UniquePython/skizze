d6d34f8eca2ede8907dd6bd68ef5f24681c89426
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-22 11:48:36 +0530

    create environment to store variables

---
+++ skizze_env.py
@@ -0,0 +1,26 @@
+from skizze_errors import SkizzeRuntimeError
+
+
+class Environment:
+    def __init__(self, parent=None):
+        self.vars = {}
+        self.parent = parent
+
+    def assign(self, name, value):
+        if name in self.vars:
+            self.vars[name] = value
+            return
+        if self.parent:
+            self.parent.assign(name, value)
+            return
+        raise SkizzeRuntimeError(f"Undefined variable '{name}'")
+
+    def get(self, name):
+        if name in self.vars:
+            return self.vars[name]
+        if self.parent:
+            return self.parent.get(name)
+        raise SkizzeRuntimeError(f"Undefined variable '{name}'")
+
+    def set(self, name, value):
+        self.vars[name] = value
